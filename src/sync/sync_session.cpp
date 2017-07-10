591a6782dba8c20197e7ee4f53ed0a87c402dfa5
Chen Mulong <chenmulong@gmail.com>
2017-07-10 12:42:43 +0800

    Fix for sync SSL related API changes
    ssl related options have been moved to Seesion::Config.
    See https://github.com/realm/realm-sync/pull/1435

--- src/sync/sync_session.cpp
+++ src/sync/sync_session.cpp
@@ -157,8 +157,7 @@ struct sync_session_states::WaitingForAccessToken : public SyncSession::State {
             session.m_session->refresh(std::move(access_token));
             session.m_session->cancel_reconnect_delay();
         } else {
-            session.m_session->bind(*session.m_server_url, std::move(access_token),
-                                    session.m_config.client_validate_ssl, session.m_config.ssl_trust_certificate_path);
+            session.m_session->bind(*session.m_server_url, std::move(access_token));
             session.m_session_has_been_bound = true;
         }
 
@@ -609,6 +608,8 @@ void SyncSession::create_sync_session()
     sync::Session::Config session_config;
     session_config.changeset_cooker = m_config.transformer;
     session_config.encryption_key = m_config.realm_encryption_key;
+    session_config.verify_servers_ssl_certificate = m_config.client_validate_ssl;
+    session_config.ssl_trust_certificate_path = m_config.ssl_trust_certificate_path;
     m_session = std::make_unique<sync::Session>(m_client.client, m_realm_path, session_config);
 
     // The next time we get a token, call `bind()` instead of `refresh()`.
