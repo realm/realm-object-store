realm/realm-core

commit 8d18a91b1bdd43784d2c0952484cca3d8a7fe845

Check all outgoing links for deep change checking
Previously it stopped after the first non-null link. It also turned out that
the check for exceeding the depth was slightly wrong in a way that was rendered
irrelevant by the first bug.

src/impl/collection_notifier.cpp
@@ -98,12 +98,12 @@ bool DeepChangeChecker::check_outgoing_links(size_t table_ndx,
         return false;
     };
 
-    for (auto const& link : it->links) {
+    auto linked_object_changed = [&](OutgoingLink const& link) {
         if (already_checking(link.col_ndx))
-            continue;
+            return false;
         if (!link.is_list) {
             if (table.is_null_link(link.col_ndx, row_ndx))
-                continue;
+                return false;
             auto dst = table.get_link(link.col_ndx, row_ndx);
             return check_row(*table.get_link_target(link.col_ndx), dst, depth + 1);
         }
@@ -115,9 +115,10 @@ bool DeepChangeChecker::check_outgoing_links(size_t table_ndx,
             if (check_row(target, dst, depth + 1))
                 return true;
         }
-    }
+        return false;
+    };
 
-    return false;
+    return std::any_of(begin(it->links), end(it->links), linked_object_changed);
 }
 
 bool DeepChangeChecker::check_row(Table const& table, size_t idx, size_t depth)
@@ -141,7 +142,7 @@ bool DeepChangeChecker::check_row(Table const& table, size_t idx, size_t depth)
         return false;
 
     bool ret = check_outgoing_links(table_ndx, table, idx, depth);
-    if (!ret && !m_current_path[depth].depth_exceeded)
+    if (!ret && (depth == 0 || !m_current_path[depth - 1].depth_exceeded))
         m_not_modified[table_ndx].add(idx);
     return ret;
 }

tests/transaction_log_parsing.cpp
@@ -1910,7 +1910,8 @@ TEST_CASE("DeepChangeChecker") {
     r->update_schema({
         {"table", {
             {"int", PropertyType::Int},
-            {"link", PropertyType::Object, "table", "", false, false, true},
+            {"link1", PropertyType::Object, "table", "", false, false, true},
+            {"link2", PropertyType::Object, "table", "", false, false, true},
             {"array", PropertyType::Array, "table"}
         }},
     });
@@ -1952,22 +1953,50 @@ TEST_CASE("DeepChangeChecker") {
     }
 
     SECTION("changes over links are tracked") {
-        r->begin_transaction();
-        for (int i = 0; i < 9; ++i)
-            table->set_link(1, i, i + 1);
-        r->commit_transaction();
+        SECTION("first link set") {
+            r->begin_transaction();
+            for (int i = 0; i < 8; ++i)
+                table->set_link(1, i, i + 1 + (i == 7));
+            r->commit_transaction();
+        }
+        SECTION("second link set") {
+            r->begin_transaction();
+            for (int i = 0; i < 8; ++i)
+                table->set_link(2, i, i + 1 + (i == 7));
+            r->commit_transaction();
+        }
+        SECTION("both set") {
+            r->begin_transaction();
+            for (int i = 0; i < 8; ++i) {
+                table->set_link(1, i, 8);
+                table->set_link(2, i, i + 1 + (i == 7));
+            }
+            r->commit_transaction();
+        }
+        SECTION("circular link") {
+            r->begin_transaction();
+            for (int i = 0; i < 8; ++i) {
+                table->set_link(1, i, i);
+                table->set_link(2, i, i + 1 + (i == 7));
+            }
+            r->commit_transaction();
+        }
 
         auto info = track_changes([&] {
             table->set_int(0, 9, 10);
         });
 
+        // link chain should cascade to all but #8 being marked as modified
         REQUIRE(_impl::DeepChangeChecker(info, *table, tables)(0));
+        REQUIRE_FALSE(_impl::DeepChangeChecker(info, *table, tables)(8));
     }
 
     SECTION("changes over linklists are tracked") {
         r->begin_transaction();
-        for (int i = 0; i < 9; ++i)
-            table->get_linklist(2, i)->add(i + 1);
+        for (int i = 0; i < 8; ++i) {
+            table->get_linklist(3, i)->add(i);
+            table->get_linklist(3, i)->add(i + 1 + (i == 7));
+        }
         r->commit_transaction();
 
         auto info = track_changes([&] {
@@ -1975,6 +2004,7 @@ TEST_CASE("DeepChangeChecker") {
         });
 
         REQUIRE(_impl::DeepChangeChecker(info, *table, tables)(0));
+        REQUIRE_FALSE(_impl::DeepChangeChecker(info, *table, tables)(8));
     }
 
     SECTION("cycles over links do not loop forever") {
@@ -1990,7 +2020,7 @@ TEST_CASE("DeepChangeChecker") {
 
     SECTION("cycles over linklists do not loop forever") {
         r->begin_transaction();
-        table->get_linklist(2, 0)->add(0);
+        table->get_linklist(3, 0)->add(0);
         r->commit_transaction();
 
         auto info = track_changes([&] {
@@ -2027,17 +2057,17 @@ TEST_CASE("DeepChangeChecker") {
         CHECK(checker2(19));
 
         _impl::DeepChangeChecker checker3(info, *table, tables);
-        CHECK(checker2(4));
-        CHECK_FALSE(checker2(3));
-        CHECK_FALSE(checker2(2));
-        CHECK(checker2(18));
-        CHECK(checker2(19));
+        CHECK(checker3(4));
+        CHECK_FALSE(checker3(3));
+        CHECK_FALSE(checker3(2));
+        CHECK(checker3(18));
+        CHECK(checker3(19));
     }
 
     SECTION("targets moving is not a change") {
         r->begin_transaction();
         table->set_link(1, 0, 9);
-        table->get_linklist(2, 0)->add(9);
+        table->get_linklist(3, 0)->add(9);
         r->commit_transaction();
 
         auto info = track_changes([&] {
@@ -2058,7 +2088,7 @@ TEST_CASE("DeepChangeChecker") {
         REQUIRE(_impl::DeepChangeChecker(info, *table, tables)(0));
 
         r->begin_transaction();
-        table->get_linklist(2, 0)->add(8);
+        table->get_linklist(3, 0)->add(8);
         r->commit_transaction();
 
         info = track_changes([&] {
@@ -2080,7 +2110,7 @@ TEST_CASE("DeepChangeChecker") {
         REQUIRE(_impl::DeepChangeChecker(info, *table, tables)(0));
 
         r->begin_transaction();
-        table->get_linklist(2, 0)->add(8);
+        table->get_linklist(3, 0)->add(8);
         r->commit_transaction();
 
         info = track_changes([&] {

realm/realm-object-store

commit 3c671560d17ac9a5e31387140f1e6b96857e5b68

Fix an off-by-one error in the tracking of depth-exceeded
Introduced in 8d18a91b1bdd43784d2c0952484cca3d8a7fe845, which adjusted which
elements were looked at but not which elements depth_exceeded was set on.

src/impl/collection_notifier.cpp
@@ -134,7 +134,7 @@ bool DeepChangeChecker::check_row(Table const& table, size_t idx, size_t depth)
     if (depth >= m_current_path.size()) {
         // Don't mark any of the intermediate rows checked along the path as
         // not modified, as a search starting from them might hit a modification
-        for (size_t i = 1; i < m_current_path.size(); ++i)
+        for (size_t i = 0; i < m_current_path.size(); ++i)
             m_current_path[i].depth_exceeded = true;
         return false;
     }