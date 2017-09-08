fcb1d7d7c909ba4df2f1bb84be9bf4722beb0abb
Christian Melchior <christian@ilios.dk>
2017-09-08 11:56:31 +0200

    Upgrade to RC18

--- src/sync/sync_session.cpp
+++ src/sync/sync_session.cpp
@@ -469,6 +469,11 @@ void SyncSession::handle_error(SyncError error)
             case ProtocolError::reuse_of_session_ident:
             case ProtocolError::bound_in_other_session:
             case ProtocolError::bad_message_order:
+            case ProtocolError::bad_changeset_header_syntax:
+            case ProtocolError::bad_changeset_size:
+            case ProtocolError::bad_changesets:
+            case ProtocolError::bad_decompression:
+
 #if REALM_SYNC_VER_MAJOR == 1
             case ProtocolError::malformed_http_request:
 #endif
