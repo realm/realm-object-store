b38f211decbbd30bd39373fb3ac26c9b36f13793
Thomas Goyne <tg@realm.io>
2020-01-31 10:07:17 -0800

    Fix a memory leak in the SyncServer test class

--- tests/util/test_file.cpp
+++ tests/util/test_file.cpp
@@ -147,6 +147,7 @@ SyncServer::SyncServer(StartImmediately start_immediately, std::string local_dir
 #else
     config.logger = new TestLogger;
 #endif
+    m_logger.reset(config.logger);
     config.log_compaction_clock = this;
 #if REALM_SYNC_VER_MAJOR > 4 || (REALM_SYNC_VER_MAJOR == 4 && REALM_SYNC_VER_MINOR >= 7)
     config.disable_history_compaction = false;
