d38637105c73a95c762a7d5782f27ed5b3222e3a
Jason Flax <jsflax@gmail.com>
2020-05-11 16:10:18 +0100

    Add Claus test case

--- tests/bson.cpp
+++ tests/bson.cpp
@@ -271,6 +271,15 @@ TEST_CASE("canonical_extjson_corpus", "[bson]") {
                 }
             });
         }
+        SECTION("Doubly Nested Array 2") {
+            run_corpus<BsonArray>("value", {
+                "{\"value\": [ [ {\"$numberInt\": \"1\"}, \"Realm\", {\"$numberInt\": \"3\"} ] ] }",
+                [](auto val) {;
+                    const BsonArray sub_array = static_cast<BsonArray>(val[0]);
+                    return sub_array.size() == 3 && sub_array[0] == 1 && sub_array[1] == "Realm" && sub_array[2] == 3;
+                }
+            });
+        }
     }
 
     SECTION("Double type") {
